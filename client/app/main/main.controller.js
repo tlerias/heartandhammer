1cb3972480c6844deaf9c9b3fc7a46bceab05161
Tara Lerias <tlerias@gmail.com>
2015-03-23 23:03:07 -0400

    images using flexbo

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -3,4 +3,22 @@
 angular.module('heartHammerApp')
   .controller('MainCtrl', function ($scope) {
 
+    // $("#phil-photo-1").hover(function () {
+    //     $('#phil-txt-1').animate({'margin-left': '700px'}, 100, function () {
+    //       $('#phil-icon-1').removeClass('hidden');
+    //     });
+    // });
+
+    // $(".tl-img-left").on('click', '#phil-icon-1', function () {
+    //     $('#phil-txt-1').animate({'margin-left': '0'}, 100, function () {
+    //       $('#phil-icon-1').addClass('hidden');
+    //     });
+    // });
+
+  // $("#main-img").hover(function () {
+  //   $('#main-info').animate({'margin-left': '700px'}, 100, function () {
+  //     // $('#phil-icon-1').removeClass('hidden');
+  //   });
+  // });
+
   });
